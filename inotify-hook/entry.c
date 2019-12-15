12b4c89c5b7de8ef5ceb5e558b56ed93539c575d
iamhyc <sudofree@163.com>
2019-12-15 15:49:38 +0800

    debug-02

--- inotify-hook/entry.c
+++ inotify-hook/entry.c
@@ -7,36 +7,15 @@
 #include <asm/tlbflush.h>
 #include <linux/sched.h>
 #include <linux/uaccess.h>
-
+#include <linux/syscalls.h>
+#include <linux/unistd.h>
 
 #define KERN_LOG KERN_NOTICE "[printk]"
 #define printh(...) printk(KERN_LOG __VA_ARGS__)
 
 void **p_sys_call_table;
 
-asmlinkage int (*ori_inotify_add_watch) (int, const char __user *, u32);
-asmlinkage int mod_inotify_add_watch(int fd, const char __user *pathname, u32 mask)
-{
-    pid_t usr_pid = current->pid;
-    char realpath[128];
-
-    //FIXME: could not read the pathname
-    if (strncpy_from_user(realpath, pathname, 128) != 0)
-    {
-        printh("could could get real pathname.");
-        return -EFAULT;
-    }
-    
-    printh("%d add watch on %s\n", usr_pid, realpath);
-    return ori_inotify_add_watch(fd, pathname, mask);
-}
-
-// asmlinkage int (*ori_inotify_rm_watch) (int, __s32);
-// asmlinkage int mod_inotify_rm_watch(int fd, __s32 wd)
-// {
-//     return ori_inotify_rm_watch(fd, wd)
-// }
-
+/*
 static void set_addr_rw(const unsigned long addr)
 {
     unsigned int level;
@@ -52,22 +31,48 @@ int set_addr_ro(unsigned long address){
     pte->pte = pte->pte &~_PAGE_RW;
     return 0;
 }
+*/
+
+asmlinkage int (*old_inotify_add_watch) (int, const char __user *, u32);
+asmlinkage int new_inotify_add_watch(int fd, const char __user *pathname, u32 mask)
+{
+/*
+    pid_t usr_pid = current->pid;
+    char realpath[128];
+
+    //FIXME: could not read the pathname
+    if (strncpy_from_user(realpath, pathname, 128) != 0)
+    {
+        printh("could could get real pathname.");
+        return -EFAULT;
+    }
+    
+    printh("%d add watch on %s\n", usr_pid, realpath);
+*/
+    printh("I hooked, and let go.");
+    return ori_inotify_add_watch(fd, pathname, mask);
+}
 
 static int __init inotify_hook_init(void)
 {
-    p_sys_call_table = (void **) kallsyms_lookup_name("sys_call_table");
-    set_addr_rw((unsigned long)p_sys_call_table);
+    write_cr0(read_cr0() & (~0x10000));
 
+    p_sys_call_table = (void **) kallsyms_lookup_name("sys_call_table");
+    //set_addr_rw((unsigned long)p_sys_call_table);
     ori_inotify_add_watch = p_sys_call_table[__NR_inotify_add_watch];
     p_sys_call_table[__NR_inotify_add_watch] = mod_inotify_add_watch;
+
+    write_cr0(read_cr0() | (0x10000));
 
     return 0;
 }
 
 static void __exit inotify_hook_fini(void)
 {
-    p_sys_call_table[__NR_inotify_add_watch] = ori_inotify_add_watch;
+    //p_sys_call_table[__NR_inotify_add_watch] = ori_inotify_add_watch;
+    write_cr0(read_cr0() & (~0x10000));
     set_addr_ro((unsigned long) p_sys_call_table);
+    write_cr0(read_cr0() | (0x10000));
     printh("inotify module exit.\n\n");
 }
 
