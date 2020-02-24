80b9fb36c1af392f66d629d27718e9dbb3a108c8
iamhyc <sudofree@163.com>
2020-02-24 20:43:17 +0800

    the plot is set.

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # VDM Vscode Plugin
 
-* The solution exists in `inotify` event register reverse lookup, however, I could not find the way for now;
-* If this successes, the method could be transferred to any editors (on *unix platform);
-* Well, now we know that there is a tool called [CRIU](https://github.com/checkpoint-restore), which is really cool.
+* The efficient solution exists in `inotify_group` reverse lookup, however, I could not find the way for now.
+* So, I hook both the `add_watch` and `rm_watch` to maintain existing watcher list for each process.
+    * With **GUI-only** and **blacklist** design, it would alleviate the overhead a lot.
+* The maintained list would be available on *proc* subsystem, and easily retrieved from userspace.
 
 ### Reference
 1. https://security.stackexchange.com/questions/210897/why-is-there-a-need-to-modify-system-call-tables-in-linux
 2. https://stackoverflow.com/questions/2103315/linux-kernel-system-call-hooking-example
 3. https://stackoverflow.com/questions/11915728/getting-user-process-pid-when-writing-linux-kernel-module
 4. https://uwnthesis.wordpress.com/2016/12/26/basics-of-making-a-rootkit-from-syscall-to-hook/
+5. https://stackoverflow.com/questions/58819136/is-it-possible-to-dump-inode-information-from-the-inotify-subsystem
--- inotify-hook/main.c
+++ inotify-hook/main.c
@@ -8,33 +8,48 @@
 
 /************************* PROTOTYPE DECLARATION *************************/
 void **p_sys_call_table;
-asmlinkage long (*ori_inotify_add_watch) (int, const char __user *, u32);
-asmlinkage long mod_inotify_add_watch(int, const char __user *, u32);
+asmlinkage int (*ori_inotify_add_watch) (int, const char __user *, u32);
+asmlinkage int mod_inotify_add_watch(int, const char __user *, u32);
+asmlinkage int (*ori_inotify_rm_watch) (int, __s32);
+asmlinkage int mod_inotify_rm_watch(int, __s32);
 static int __init inotify_hook_init(void);
 static void __exit inotify_hook_fini(void);
 
 /*************************** INOTIFY_ADD_WATCH ***************************/
-asmlinkage long mod_inotify_add_watch(int fd, const char __user *pathname, u32 mask)
+asmlinkage int mod_inotify_add_watch(int fd, const char __user *pathname, u32 mask)
 {
-    long ret;
+    int wd;
     struct path path;
     unsigned int flags = 0;
     char *pname = NULL;
     char buf[PATH_MAX];
     pid_t usr_pid = task_pid_nr(current);
 
-    ret = ori_inotify_add_watch(fd, pathname, mask);
+    wd = ori_inotify_add_watch(fd, pathname, mask);
 
     if (!(mask & IN_DONT_FOLLOW))
 		flags |= LOOKUP_FOLLOW;
 	if (mask & IN_ONLYDIR)
 		flags |= LOOKUP_DIRECTORY;
     
-    if (user_path_at(AT_FDCWD, pathname, flags, &path) == 0)
+    if (wd>0 && user_path_at(AT_FDCWD, pathname, flags, &path)==0)
     {
         pname = dentry_path_raw(path.dentry, buf, PATH_MAX);
-        printh("%d, %s\n", usr_pid, pname);
+        path_put(&path);
+        printh("[%d] (%d %d + %s) \n", usr_pid, fd, wd, pname);
     }
+
+    return wd;
+}
+/*************************** INOTIFY_RM_WATCH ***************************/
+asmlinkage int mod_inotify_rm_watch(int fd, __s32 wd)
+{
+    int ret;
+    pid_t usr_pid = task_pid_nr(current);
+
+    ret = ori_inotify_rm_watch(fd, wd);
+
+    printh("[%d] (%d %d - )\n", usr_pid, fd, wd);
 
     return ret;
 }
@@ -54,8 +69,11 @@ static int __init inotify_hook_init(void)
     set_addr_rw((unsigned long)p_sys_call_table);
     ori_inotify_add_watch = p_sys_call_table[__NR_inotify_add_watch];
     p_sys_call_table[__NR_inotify_add_watch] = mod_inotify_add_watch;
+    ori_inotify_rm_watch = p_sys_call_table[__NR_inotify_rm_watch];
+    p_sys_call_table[__NR_inotify_rm_watch] = mod_inotify_rm_watch;
     set_addr_ro((unsigned long)p_sys_call_table);
 
+    printh("inotify module init.\n");
     return 0;
 }
 
@@ -63,6 +81,7 @@ static void __exit inotify_hook_fini(void)
 {
     set_addr_rw((unsigned long)p_sys_call_table);
     p_sys_call_table[__NR_inotify_add_watch] = ori_inotify_add_watch;
+    p_sys_call_table[__NR_inotify_rm_watch]  = ori_inotify_rm_watch;
     set_addr_ro((unsigned long)p_sys_call_table);
     printh("inotify module exit.\n\n");
 }
