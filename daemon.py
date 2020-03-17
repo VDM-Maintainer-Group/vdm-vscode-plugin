b32096ac484ba789bd8114ab6dfa49f7a143406d
iamhyc <sudofree@163.com>
2020-03-17 23:40:04 +0800

    fix userspace program

--- daemon.py
+++ daemon.py
@@ -1,8 +1,20 @@
 #!/usr/bin/env python3
+''' Userspace Inotify Lookup Daemon Program
+References:
+    1. https://github.com/est/pymnl/blob/master/pymnl/nlsocket.py
+    2. https://stackoverflow.com/questions/35876323/python-netlink-multicast-communication-in-kernels-above-4
+'''
 import os, time, struct
 import socket
 from sys import argv
 from gnlpy import netlin
+
+SOL_NETLINK = 270
+NETLINK_ADD_MEMBERSHIP  = 1
+NETLINK_DROP_MEMBERSHIP = 2
+NETLINK_PKTINFO         = 3
+NETLINK_BROADCAST_ERROR = 4
+NETLINK_NO_ENOBUFS      = 5
 
 KERNEL_NAME='inotify_hook'
 req_attr = netlink.create_attr_list_type('req',
@@ -19,6 +31,8 @@ msg_format = netlink.create_genl_message_type('Msg', KERNEL_NAME
 def main(pid=argv[1]):
     sockfd = socket.socket(socket.AF_NETLINK, socket.SOCK_RAW, socket.NETLINK_USERSOCK)
     sockfd.bind((os.getpid(), 0))
+    # 270 is SOL_NETLINK and 1 is NETLINK_ADD_MEMBERSHIP
+    sock.setsockopt(270, NETLINK_ADD_MEMBERSHIP, 31)
     sockfd.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, 65536)
     sockfd.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, 65536)
 
--- inotify-hook/netlink_comm.h
+++ inotify-hook/netlink_comm.h
@@ -1,6 +1,13 @@
 #ifndef __NETLINK_COMM_H__
 #define __NETLINK_COMM_H__
 
+#include <net/sock.h>
+#include <linux/socket.h>
+#include <linux/net.h>
+#include <asm/types.h>
+#include <linux/netlink.h>
+#include <linux/skbuff.h>
 
+#define NETLINK_USER 31
 
 #endif
