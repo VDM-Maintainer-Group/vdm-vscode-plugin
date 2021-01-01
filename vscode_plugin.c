0219376b38b08f815692ae0d4111c261bc819005
iamhyc <sudofree@163.com>
2021-01-01 23:13:26 +0800

    add temp src interface

--- vscode_plugin.c
+++ vscode_plugin.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
-#include "inotify_lookup.h"
-// #include <vdm/capability/inotify_lookup.h>
+// #include <vdm/interface/src_api.h>
+#include <vdm/capability/inotify_lookup.h>
 
-int main(int argc, char const *argv[])
+/* Custom Interface */
+int onTrigger(void *args)
 {
-    int ret, pos;
-    char **result;
+    return 0;
+}
 
-    ret = inotify_lookup_register("code");
-    printf("add with ret code: %d.\n", ret);
+/* SRC API Interface */
+int onSave(char *stat_file)
+{
+    int pos;
+    char **result;
 
     result = inotify_lookup_dump("code");
     while (pos<MAX_DUMP_LEN && result[pos])
@@ -18,8 +23,43 @@ int main(int argc, char const *argv[])
         pos ++;
     }
 
-    // ret = inotify_lookup_unregister("code");
-    // printf("rm with ret code: %d.\n", ret);
-
     return 0;
+}
+
+int onResume(char *stat_file)
+{
+    return 0;
+}
+
+int onClose()
+{
+    return 0;
+}
+
+/* Common Startup */
+int onStart()
+{
+    int ret;
+    ret = inotify_lookup_register("code");
+    return ret;
+}
+
+int onStop()
+{
+    int ret;
+    ret = inotify_lookup_unregister("code");
+    return ret;
+}
+
+int main(int argc, char const *argv[])
+{
+    int ret;
+
+    ret = inotify_lookup_register("code");
+    printf("add with ret code: %d.\n", ret);
+
+    ret = inotify_lookup_unregister("code");
+    printf("rm with ret code: %d.\n", ret);
+
+    return ret;
 }
