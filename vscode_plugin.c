d7df0e1c9a1068104e5ddc938b76b43e081c5365
iamhyc <sudofree@163.com>
2021-01-15 17:31:26 +0800

    update funcions

--- vscode_plugin.c
+++ vscode_plugin.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-// #include <vdm/interface/src_api.h>
+#include <string.h>
+#include <vdm/interface/src_api.h>
 #include <vdm/capability/inotify_lookup.h>
 
 /* Custom Interface */
@@ -11,28 +12,57 @@ int onTrigger(void *args)
 }
 
 /* SRC API Interface */
-int onSave(char *stat_file)
+int onSave(const char *stat_file)
 {
     int pos;
     char **result;
+    FILE *fd;
+    char *ref, *pid, *path;
+
+    if ( (fd=fopen(stat_file, "w"))==NULL )
+    {
+        return -1;
+    }
 
     result = inotify_lookup_dump("code");
     while (pos<MAX_DUMP_LEN && result[pos])
     {
+        ref = result[pos];
+        pid = strtok(ref, ",");
+        path = strtok(NULL, ",");
+        fprintf(fd, "%s\n", path);
+        //
         printf("dump [%d]: %s\n", pos, result[pos]);
         pos ++;
+    }
+    free(result);
+    
+    return 0;
+}
+
+int onResume(const char *stat_file)
+{
+    FILE *fd;
+    char buf[1024];
+    char command[255];
+    
+    if ( (fd=fopen(stat_file, "r"))==NULL )
+    {
+        return -1;
+    }
+
+    while ( fscanf(fd, "%s", &buf)==1 )
+    {
+        sprintf(command, "code %s", buf);
+        system(command);
     }
 
     return 0;
 }
 
-int onResume(char *stat_file)
-{
-    return 0;
-}
-
 int onClose()
 {
+    system("killall code");
     return 0;
 }
 
@@ -42,7 +72,6 @@ int onStart()
     int ret;
     ret = inotify_lookup_register("code");
     return ret;
-}
 
 int onStop()
 {
@@ -55,10 +84,10 @@ int main(int argc, char const *argv[])
 {
     int ret;
 
-    ret = inotify_lookup_register("code");
+    ret = onStart();
     printf("add with ret code: %d.\n", ret);
 
-    ret = inotify_lookup_unregister("code");
+    ret = onStop();
     printf("rm with ret code: %d.\n", ret);
 
     return ret;
