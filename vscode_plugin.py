cdc37b328cc72e8f7199b85755d89874bc19b0d1
iamhyc <sudofree@163.com>
2022-05-31 21:27:25 +0800

    update with window control

--- vscode_plugin.py
+++ vscode_plugin.py
@@ -1,42 +1,95 @@
 #!/usr/bin/env python3
-import os, psutil
-import time
+import os, re, time, psutil
+import json
 from pathlib import Path
 from pyvdm.interface import CapabilityLibrary, SRC_API
 
-BLACKLIST = ['/Code/User/settings.json', '/.vscode/settings.json', '/.git/refs/remotes/']
+DBG = 0
+
+BLACKLIST = ['/Code/User/settings.json', '/.vscode/settings.json', '/.git/refs/remotes/',
+            '/usr/lib/', '/usr/local/lib/',
+            '/lib/python',
+            '/target/rls/']
 VALIDATE  = lambda x: ( True not in [_ in x for _ in BLACKLIST] )
+
+WINDOW_TITLE = '%s - Visual Studio Code'
 
 class VscodePlugin(SRC_API):
     @staticmethod
-    def _gather_record(raw_result):
-        record = dict()
+    def _gather_records(raw_result):
+        records = list()
+
+        ## collect and filter the raw data
+        _processes = dict()
         for item in raw_result:
             pid, path = item.split(',', maxsplit=1)
-            path = path.rstrip('\u0000')
-            #
-            if pid not in record.keys():
-                record[pid] = [path]
-            else:
-                record[pid].append(path)
+            if VALIDATE(path):
+                pid  = int(pid)
+                path = path.rstrip('\u0000')
+                #
+                if pid not in _processes.keys():
+                    _processes[pid] = [path]
+                else:
+                    _processes[pid].append(path)
             pass
-        return record
+        # if DBG: print( json.dumps(_processes[15969], indent=4) )
 
-    @staticmethod
-    def _dump_record(fh, record):
-        for pid, items in record.items():
-            pid = int(pid)
-            if not psutil.pid_exists(pid):
+        ## gather records
+        for pid, items in _processes.items():
+            if not psutil.pid_exists(pid): #maybe workspace
                 _path = os.path.commonpath(items)
-                if _path: fh.write(_path+'\n') #fh.write( 'workspace %s'%_path )
-            elif '--type=fileWatcher' in psutil.Process(pid).cmdline():
+                records.append( _path )
+                if DBG: print(f'workspace [{pid}]: {_path}')
+            elif '--type=fileWatcher' in psutil.Process(pid).cmdline(): #maybe files
                 for _path in items:
                     if VALIDATE(_path) and Path(_path).is_file():
-                        fh.write(_path+'\n') #fh.write( 'file %s'%_path )
+                        records.append( _path )
+                        if DBG: print(f'file [{pid}]: {_path}')
+            pass
+
+        return records
+    
+    def _associate_with_window(self, records):
+        results = dict()
+
+        windows = self.xm.get_windows_by_name('Visual Studio Code')
+        windows.sort(key=lambda x:x['xid'])
+
+        for _path in records:
+            _name  = Path(_path).name
+            _title = WINDOW_TITLE%_name
+            for w in windows:
+                if _title in w['name']:
+                    results[_name] = {
+                        'path': _path,
+                        'window': {
+                            'desktop':w['desktop'],
+                            'states':w['states'],
+                            'xyhw':w['xyhw']
+                        }
+                    }
+                    continue
+            pass
+
+        return results
+
+    def _rearrange_window(self, records):
+        windows = self.xm.get_windows_by_name('Visual Studio Code')
+        windows.sort(key=lambda x:x['xid'])
+
+        for name,status in records.items():
+            _title = WINDOW_TITLE%name
+            for w in windows:
+                if _title in w['name']:
+                    s = status['window']
+                    self.xm.set_window_by_xid( w['xid'],
+                        s['desktop'], s['states'], s['xyhw'] )
+                    continue
             pass
         pass
 
     def onStart(self):
+        self.xm = CapabilityLibrary.CapabilityHandleLocal('x11-manager')
         self.il = CapabilityLibrary.CapabilityHandleLocal('inotify-lookup')
         self.il.register('code')
         return 0
@@ -46,23 +99,37 @@ class VscodePlugin(SRC_API):
         return 0
 
     def onSave(self, stat_file):
-        # dump raw_result via il
-        raw_result = self.il.dump('code')
-        # gathering record from raw_result
-        record = self._gather_record(raw_result)
-        # write to file
+        ## dump raw_result via il
+        raw_results = self.il.dump('code')
+        ## gathering record from raw_results
+        records = self._gather_records(raw_results)
+        ## associate with X window
+        records = self._associate_with_window(records)
+        ## write to file
         with open(stat_file, 'w') as f:
-            self._dump_record(f, record)
+            json.dump(records, f)
             pass
         return 0
 
     def onResume(self, stat_file):
+        ## load stat file with failure check
         with open(stat_file, 'r') as f:
-            items = f.readlines()
-            for piece in items:
-                os.system( 'code --new-window %s'%piece )
-            pass
-        time.sleep(1.5)
+            _file = f.read().strip()
+        if len(_file)==0:
+            return 0
+        else:
+            try:
+                records = json.loads(_file)
+            except:
+                return -1
+        ## open windows and wait for title
+        for item in records:
+            os.system( 'code --new-window %s'%item['path'] )
+        pass
+        time.sleep( len(records) * 0.60 )
+        ## rearrange windows by title
+        self._rearrange_window(records)
+        time.sleep(0.5)
         return 0
 
     def onClose(self):
@@ -71,15 +138,13 @@ class VscodePlugin(SRC_API):
     pass
 
 if __name__ == '__main__':
-    import sys, json
     _plugin = VscodePlugin()
     _plugin.onStart()
 
-    # dump raw_result via il
-    raw_result = _plugin.il.dump('code')
-    # gathering record from raw_result
-    record = _plugin._gather_record(raw_result)
-    # print( json.dumps(record, indent=4) )
-    # write to file
-    _plugin._dump_record(sys.stdout, record)
+    raw_results = _plugin.il.dump('code')
+    ## gathering record from raw_results
+    records = _plugin._gather_records(raw_results)
+    ## associate with X window
+    records = _plugin._associate_with_window(records)
+    print( json.dumps(records, indent=4) )
     pass
